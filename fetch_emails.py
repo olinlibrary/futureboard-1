b878de0fa47d9b935eb77bcfd3037531d4c5b3cc
SeanFoley123 <sean.foley@students.olin.edu>
2017-03-28 23:16:36 -0400

    Fetching emails. Next will add them to current data files, the current JSON, and the database. Not sure if all that is necessary, though; could we just skip to the database?

--- fetch_emails.py
+++ fetch_emails.py
@@ -9,10 +9,10 @@ from pprint import PrettyPrinter
 pp = PrettyPrinter()
 
 try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+	import argparse
+	flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
-    flags = None
+	flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/gmail-python-quickstart.json
@@ -22,57 +22,68 @@ APPLICATION_NAME = 'Gmail API Python Quickstart'
 
 
 def get_credentials():
-    """Gets valid user credentials from storage.
+	"""Gets valid user credentials from storage.
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+	If nothing has been stored, or if the stored credentials are invalid,
+	the OAuth2 flow is completed to obtain the new credentials.
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+	Returns:
+		Credentials, the obtained credential.
+	"""
+	home_dir = os.path.expanduser('~')
+	credential_dir = os.path.join(home_dir, '.credentials')
+	if not os.path.exists(credential_dir):
+		os.makedirs(credential_dir)
+	credential_path = os.path.join(credential_dir,
+								   'gmail-python-quickstart.json')
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+	store = Storage(credential_path)
+	credentials = store.get()
+	if not credentials or credentials.invalid:
+		flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+		flow.user_agent = APPLICATION_NAME
+		if flags:
+			credentials = tools.run_flow(flow, store, flags)
+		else: # Needed only for compatibility with Python 2.6
+			credentials = tools.run(flow, store)
+		print('Storing credentials to ' + credential_path)
+	return credentials
 
 def main():
-    """Shows basic usage of the Gmail API.
+	"""Shows basic usage of the Gmail API.
 
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
-    """
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
+	Creates a Gmail API service object and outputs a list of label names
+	of the user's Gmail account.
+	"""
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
+	service = discovery.build('gmail', 'v1', http=http)
 
-    results = service.users().messages().list(userId='me').execute()
-    # pp.pprint(results)
-    for msg_id in results['messages']:
-    	message = service.users().messages().get(userId='me', id=msg_id['id'], format="raw").execute()
-    	pp.pprint((message["raw"]))
-    # labels = results.get('messages', [])
+	results = service.users().messages().list(userId='me').execute()
+	# pp.pprint(results)
+	for msg_id in results['messages']:
+		message = service.users().messages().get(userId='me', id=msg_id['id']).execute()
+		try:
+			pp.pprint(message["payload"]['parts'])
+		except:
+			pass
 
-    # if not labels:
-    #     print('No labels found.')
-    # else:
-    #   print('Labels:')
-    #   for label in labels:
-    #     print(label['name'])
+
+		# if "parts" not in message["payload"].keys():
+		# 	subject = next(meta["value"] for meta in message["payload"]["headers"] if meta['name'] == "Subject")
+		# 	print(subject)
+		# 	# str(base64.b64decode(
+		# else:
+		# 	print("Message contains HTML elements")
+	# labels = results.get('messages', [])
+
+	# if not labels:
+	#     print('No labels found.')
+	# else:
+	#   print('Labels:')
+	#   for label in labels:
+	#     print(label['name'])
 
 
 if __name__ == '__main__':
-    main()
+	main()
