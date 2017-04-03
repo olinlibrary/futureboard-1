feec669694655658bac14da48d95f8568721504d
SeanFoley123 <sean.foley@students.olin.edu>
2017-04-03 19:14:04 -0400

    Broke apart functions

--- fetch_emails.py
+++ fetch_emails.py
@@ -6,6 +6,7 @@ from oauth2client import tools
 from oauth2client.file import Storage
 from pprint import PrettyPrinter
 from wrangle import update_jsons
+from binascii import Error
 pp = PrettyPrinter()
 
 try:
@@ -59,6 +60,43 @@ def get_credentials():
         credentials = request_live_credentials(store, credential_path)
     return credentials
 
+def parse_email(message):
+    email_content = {}
+
+    email_content["id"] = message.get('id', False)
+    meta_dict = {meta["name"].lower(): meta["value"] for meta in message["payload"]["headers"] if meta["name"] in ["Subject", "Date", "From"]}
+    email_content["subject"] = meta_dict.get("subject", False)
+    email_content["date"] = meta_dict.get("date", False)
+
+    # From format is 'John Doe <johny_appleseed@gmail.com>'
+    name = re.findall(re.compile('[a-zA-Z]{1,} [a-zA-Z]{1,}'), meta_dict.get("from", ""))
+    email = re.findall(re.compile('<.*>'), meta_dict.get("from", ""))
+    email_content["author_name"] = name[0] if name else False
+    email_content["author_email"] = email[0].lstrip("<").rstrip(">") if email else False
+    email_content["replying_to"] = False
+    
+    try:
+        try:
+            body = message['payload']['parts'][0]['parts'][0]['body']['data']
+        except KeyError:
+            body = message['payload']['body']['data']
+    except KeyError:
+        body = message['payload']['parts'][0]['parts'][0]['parts'][0]['body']['data']
+    
+    missing_padding = len(body) % 4
+    if missing_padding != 0:
+        body += str(b'='* (4 - missing_padding))
+
+    try:
+        email_content['text'] = base64.b64decode(body).decode(errors="ignore")      # Having unicode errors
+    except Error:
+        email_content['text'] = "Corrupted Data"
+        print("Email "+email_content['id']+": "+email_content['subject']+" is corrupted")
+    # print(email_content['subject'])
+    # print(email_content['text'])
+    # print(' ')
+    return email_content
+
 
 def main():
     """Connects to gmail account and pulls out all new emails to the carpe list, then separates them
@@ -69,47 +107,27 @@ def main():
     service = discovery.build('gmail', 'v1', http=http)
 
     email_stored = "Label_1"
-    results = service.users().messages().list(userId='me', q="to:carpediem@lists.olin.edu NOT label:{}".format(email_stored)).execute()
-
+    results = service.users().messages().list(userId='me', q="to:carpediem@lists.olin.edu".format(email_stored)).execute()
+    print(results)
     emails = {}
-    response = service.users().labels().list(userId='me').execute()
-    for msg_id in results['messages']:
-        message = service.users().messages().get(userId='me', id=msg_id['id']).execute()
-        service.users().messages().modify(userId="me", id=msg_id['id'], body={'removeLabelIds': [], 'addLabelIds': [email_stored]}).execute()
+    if results['resultSizeEstimate']:
+        for result in results['messages']:
+            msg_id = result['id']
+            message = service.users().messages().get(userId='me', id=msg_id).execute()
+            service.users().messages().modify(userId="me", id=msg_id, body={'removeLabelIds': [], 'addLabelIds': [email_stored]}).execute()
 
-        email_content = {}
+            email_content = parse_email(message)
 
-        email_content["id"] = message.get('id', False)
-        meta_dict = {meta["name"].lower(): meta["value"] for meta in message["payload"]["headers"] if meta["name"] in ["Subject", "Date", "From"]}
-        email_content["subject"] = meta_dict.get("subject", False)
-        email_content["date"] = meta_dict.get("date", False)
-
-        # From format is 'John Doe <johny_appleseed@gmail.com>'
-        name = re.findall(re.compile('[a-zA-Z]{1,} [a-zA-Z]{1,}'), meta_dict.get("from", ""))
-        email = re.findall(re.compile('<.*>'), meta_dict.get("from", ""))
-        email_content["author_name"] = name[0] if name else False
-        email_content["author_email"] = email[0].lstrip("<").rstrip(">") if email else False
-
-        email_content["replying_to"] = False
-        pp.pprint(message['payload'])
-        try:
-            body = message['payload']['parts'][0]['parts'][0]['body']['data'].rstrip("=") + "=="
-        except KeyError:
-            body = message['payload']['body']['data'].rstrip("=") + "=="
-        email_content['text'] = base64.b64decode(body).decode(errors="ignore")      # Having unicode errors
-
-        month = re.findall(re.compile("[A-Z]{1}[a-z]{2} [0-9]{4}"), meta_dict.get("date", ''))
-        month = month[0]
-        try:
-            emails[month].append(email_content)
-        except KeyError:
-            emails[month] = [email_content]
-
-    if not emails:
-        print("No new emails!")
-    else:
+            month = re.findall(re.compile("[A-Z]{1}[a-z]{2} [0-9]{4}"), email_content.get("date", ''))[0]
+            
+            try:
+                emails[month].append(email_content)
+            except KeyError:
+                emails[month] = [email_content]
         for date, lst_emails in emails.items():
             update_jsons(lst_emails, date)
+    else:
+        print("No new emails!")        
 
 if __name__ == '__main__':
     main()
